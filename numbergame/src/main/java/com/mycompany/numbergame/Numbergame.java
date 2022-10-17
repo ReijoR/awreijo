a44fee5fa35e75a757b19c36254b9c68618562ce
Reijo <reijo.ruoho@gmail.com>
2022-10-17 15:57:17 +0300

    Eka tehtävä

---
+++ numbergame/src/main/java/com/mycompany/numbergame/Numbergame.java
@@ -0,0 +1,39 @@
+
+        
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.numbergame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Reijo
+ */
+public class Numbergame {
+
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner =  new Scanner(System.in);
+        int secret=(int)(Math.random()*100+1);
+        int guess = 0;
+            while (secret != guess) {
+            System.out.println("Number guess 1-100");
+            guess = scanner.nextInt();
+            if (guess > secret)
+                System.out.println("Number is lower");
+            if (guess < secret)
+                    System.out.println("Number is higher");
+            
+            
+            
+                
+        }
+            System.out.println("Right answer!");
+    }
+}
